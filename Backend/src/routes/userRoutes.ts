e52e7ee6026d60597b1d4691a87137376328e2c9
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-22 03:59:27 +0530

    userRoutes.ts

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { userController } from "../controllers/userController";
-
+import { protect } from "../middleware/auth";
 const router = express.Router();
 
 // Route to send OTP
@@ -15,5 +15,12 @@ router.post("/signup", userController.signup);
 // Route to login
 
 router.post("/login", userController.login);
+
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset-password", userController.resetPassword);
+
+router.get("/me",protect, userController.getMe);
+
+router.put("/update-profile",protect, userController.updateProfile);
 
 export default router;
