cfa8fe6c0811b36bebef2dffb3c2b7bc24602f84
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-21 23:43:16 +0530

    login work

--- Backend/src/controllers/userController.ts
+++ Backend/src/controllers/userController.ts
@@ -2,7 +2,9 @@ import { Request, Response, NextFunction } from "express";
 import { User, IUser, IUserInput } from "../models/User";
 import { OTP } from "../models/OTP";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { emailService } from "../utils/emailService";
+import { config } from "../config/env";
 
 // Generate and send OTP
 const sendOTP = async (
@@ -124,8 +126,65 @@ const signup = async (
   }
 };
 
+// login controller functionality
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    // check the existance of the email and password fields
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password required.." });
+    }
+    // find the user by email if does not exist
+
+    const user = await User.findOne({ email }).select("+password");
+    if (!user || !user.password) {
+      res.status(404).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    // if everything is valid return the user
+    const payload = {
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+    };
+    console.log(user.role);
+    // generate the token
+    const token = jwt.sign(payload, config.jwtSecret, {
+      expiresIn: config.jwtExpire,
+    });
+
+    user.token = token;
+    user.password = undefined;
+
+    res
+      .cookie("token", token, {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json({
+        message: "User logged in successfully",
+        success: true,
+        token,
+        user,
+      });
+    console.log(user);
+  } catch (error) {
+    res.status(500).json({ message: "error while logging in", error: error });
+  }
+};
+
 // Export the controller functions
 export const userController = {
   sendOTP,
   signup,
+  login,
 };
