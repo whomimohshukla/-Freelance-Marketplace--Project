552a15f289c2730eccdea701e44736645148211d
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-22 00:36:21 +0530

    login and signup

--- Backend/src/routes/userRoutes.ts
+++ Backend/src/routes/userRoutes.ts
@@ -1,12 +1,19 @@
-import express from 'express';
-import { userController } from '../controllers/userController';
+import express from "express";
+import { userController } from "../controllers/userController";
 
 const router = express.Router();
 
 // Route to send OTP
-router.post('/send-otp', userController.sendOTP);
+router.post("/send-otp", userController.sendOTP);
+
+// Route to resend OTP
+router.post("/resend-otp", userController.resendOTP);
 
 // Route to verify OTP and complete signup
-router.post('/signup', userController.signup);
+router.post("/signup", userController.signup);
+
+// Route to login
+
+router.post("/login", userController.login);
 
 export default router;
