f5a660975afdca5343c96cf73982adba14fc7cab
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-12-22 20:06:49 +0530

    commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Home from './pages/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
 
 function App() {
   return (
-    <div className="min-h-screen bg-primary flex flex-col">
-      <Navbar />
-      <main className="flex-grow">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen bg-primary">
+        <Navbar />
+        <main className="flex-grow pt-[72px]"> 
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
