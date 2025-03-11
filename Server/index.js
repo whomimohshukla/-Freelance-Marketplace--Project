2e2733753272f2e890f69ca953af79059de478da
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-03-11 23:42:56 +0530

    Project crud

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const database = require("../Server/config/database");
 const userRoutes = require("../Server/routes/user.routes");
+const projectRoutes = require("../Server/routes/projectCRUD.routes");
+const skillsRoutes=require("../Server/routes/skills.routes")
 
 app.use(express.json());
 app.use(cookieParser());
@@ -11,9 +13,11 @@ app.use(cookieParser());
 // connect db
 database.connectDB();
 
-// mount route to server 
+// mount route to server
 app.use("/api/v1/users", userRoutes);
+app.use("/api/project", projectRoutes);
+app.use("/api/skills",skillsRoutes)
 
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+	console.log(`server is running on ${PORT}`);
 });
