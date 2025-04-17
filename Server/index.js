ea33383cef73107675ac3156dad225207bc3be30
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-04-18 00:56:22 +0530

    team controller crud

--- Server/index.js
+++ Server/index.js
@@ -5,10 +5,11 @@ const PORT = process.env.PORT || 3000;
 const database = require("../Server/config/database");
 const userRoutes = require("../Server/routes/user.routes");
 const projectRoutes = require("../Server/routes/projectCRUD.routes");
-const skillsRoutes=require("../Server/routes/skills.routes")
-const freelancersRoute=require("../Server/routes/freelancer.routes")
-const clientRoute=require("../Server/routes/client.routes")
-const industryRoutes=require("../Server/routes/industry.routes")
+const skillsRoutes = require("../Server/routes/skills.routes")
+const freelancersRoute = require("../Server/routes/freelancer.routes")
+const clientRoute = require("../Server/routes/client.routes")
+const industryRoutes = require("../Server/routes/industry.routes")
+const teamRoutes = require('../Server/routes/team.routes');
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,10 +20,12 @@ database.connectDB();
 // mount route to server
 app.use("/api/v1/users", userRoutes);
 app.use("/api/project", projectRoutes);
-app.use("/api/skills",skillsRoutes)
-app.use("/api/freelancers",freelancersRoute )
-app.use("/api/clients",clientRoute)
-app.use("/api/industries",industryRoutes)
+app.use("/api/skills", skillsRoutes)
+app.use("/api/freelancers", freelancersRoute)
+app.use("/api/clients", clientRoute)
+app.use("/api/industries", industryRoutes)
+
+app.use('/api/teams', teamRoutes);
 
 app.listen(PORT, () => {
 	console.log(`server is running on ${PORT}`);
