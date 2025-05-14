d32064b097c9dabb21d2fc6e372ba188e7c82b65
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-05-14 22:11:46 +0530

    project crud

--- Server/routes/projectCRUD.routes.js
+++ Server/routes/projectCRUD.routes.js
@@ -1,9 +1,17 @@
-const Project = require("../controllers/core-Project/Project-crud/projectCRUD.controller");
+// const Project = require("../controllers/core-Project/Project-crud/projectCRUD.controller");
+const Project=require("../controllers/projects/project.controller")
 const express = require("express");
 const auth = require("../middleware/auth.middleware");
 const Router = express.Router();
 
+// Router.post("/create", auth, Project.createProject);
+
+
 Router.post("/create", auth, Project.createProject);
+
+Router.get("/",auth,Project.getAllProjects)
+
+
 
 
 module.exports = Router;
